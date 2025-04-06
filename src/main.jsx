47816ab3109797e8f39d9280c85dcd467459935e
aman kanu <amankanu845@gmail.com>
2025-04-06 21:38:15 +0530

    Initial commit for frontend

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+
+import {BrowserRouter} from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import AppContextProvider from './context/appcontext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+    <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+    </BrowserRouter>
+  
+)
