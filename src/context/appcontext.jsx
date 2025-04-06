7d567078a2ca308bf714e7ea61ea8ae98fb1c6ce
aman kanu <amankanu845@gmail.com>
2025-04-06 22:40:30 +0530

    chore: update backend URL to Render deployment

--- src/context/appcontext.jsx
+++ src/context/appcontext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) =>{
     const [token , setToken] = useState(localStorage.getItem('token'))
     const [credit ,setCredit] = useState(false)
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://ai-image-generator-backend-y663.onrender.com"
 
     const navigate = useNavigate()
 
